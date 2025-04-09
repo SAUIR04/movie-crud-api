ddbd64e123f102e559c55ce1dabd884105459bf2
Sauirbay <sAuirbau>
2025-04-09 13:04:57 +0500

    commit image

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -4,6 +4,7 @@ type Movie struct {
 	ID          uint   `json:"id" gorm:"primaryKey"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	imageURL    string `json:"image_url"`
 	Genre       string `json:"genre"`
 	ReleaseDate string `json:"release_date"`
 }
