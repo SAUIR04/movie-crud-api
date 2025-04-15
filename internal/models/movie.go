e2d09b08341c129f60e03503c7bc2d916065e724
Sauirbay <sAuirbau>
2025-04-15 16:40:07 +0500

    9 week migrations

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -1,7 +1,7 @@
 package models
 
 type Movie struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
+	ID          uint   `gorm:"primaryKey" json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	ImageURL    string `json:"image_url"`
